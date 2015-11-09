e833ed2c979f2faa164b28fb40729285e562f579
Bruce Daniel <r00t@blackbook.systems>
2015-11-09 13:23:54 -0800

    search path futzing

--- crypto/bb_ntru_poc/bb_ntru_poc/main.c
+++ crypto/bb_ntru_poc/bb_ntru_poc/main.c
@@ -7,10 +7,68 @@
 //
 
 #include "ntru.h"
+#include "encparams.h"
 #include <stdio.h>
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    printf("bb:  Hello, World!\n");
+//    printf("bb:  Hello, World!\n");
+    
+    
+    
+    struct NtruEncParams params = NTRU_DEFAULT_PARAMS_128_BITS; /*see encparams.h for more*/
+    NtruRandGen rng_def = NTRU_RNG_DEFAULT;
+    NtruRandContext rand_ctx_def;
+    if (ntru_rand_init(&rand_ctx_def, &rng_def) != NTRU_SUCCESS)
+        printf("rng fail\n");
+    NtruEncKeyPair kp;
+    if (ntru_gen_key_pair(&params, &kp, &rand_ctx_def) != NTRU_SUCCESS)
+        printf("keygen fail\n");
+    
+    /* deterministic key generation from password */
+    uint8_t seed[17];
+    strcpy(seed, "my test password");
+    NtruRandGen rng_igf2 = NTRU_RNG_IGF2;
+    NtruRandContext rand_ctx_igf2;
+    if (ntru_rand_init_det(&rand_ctx_igf2, &rng_igf2, seed, strlen(seed)) != NTRU_SUCCESS)
+        printf("rng fail\n");
+    if (ntru_gen_key_pair(&params, &kp, &rand_ctx_igf2) != NTRU_SUCCESS)
+        printf("keygen fail\n");
+    
+    /* encryption */
+    uint8_t msg[9];
+    strcpy(msg, "whatever");
+    uint8_t enc[ntru_enc_len(&params)];
+    if (ntru_encrypt(msg, strlen(msg), &kp.pub, &params, &rand_ctx_def, enc) != NTRU_SUCCESS)
+        printf("encrypt fail\n");
+    
+    /* release RNG resources */
+    if (ntru_rand_release(&rand_ctx_def) != NTRU_SUCCESS)
+        printf("rng fail\n");
+    if (ntru_rand_release(&rand_ctx_igf2) != NTRU_SUCCESS)
+        printf("rng fail\n");
+    
+    /* decryption */
+    uint8_t dec[ntru_max_msg_len(&params)];
+    uint16_t dec_len;
+    if (ntru_decrypt((uint8_t*)&enc, &kp, &params, (uint8_t*)&dec, &dec_len) != NTRU_SUCCESS)
+        printf("decrypt fail\n");
+    
+    /* export key to uint8_t array */
+    uint8_t pub_arr[ntru_pub_len(&params)];
+    ntru_export_pub(&kp.pub, pub_arr);
+    
+    /* import key from uint8_t array */
+    NtruEncPubKey pub;
+    ntru_import_pub(pub_arr, &pub);
+    
+    
+    
+    
+    
+    
+    
     return 0;
 }
+
+

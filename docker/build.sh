ff4d2eed72eb3914c0d25fadd3e38bd982a9ee5f
root <root@fervid-kepler.c.alert-impulse-113916.internal>
2015-11-24 16:57:06 +0000

    added ghost and lychee

--- docker/build.sh
+++ docker/build.sh
@@ -2,3 +2,5 @@ docker build -t blackbook/cloud9 cloud9
 docker build -t blackbook/etherpad etherpad
 docker build -t blackbook/ethercalc ethercalc
 docker build -t blackbook/proxy proxy
+docker build -t blackbook/lychee lychee
+docker build -t blackbook/ghost ghost
